e23d9098a1fc6abd301bc61650f8ea314a3be104
Nicolas_note <nicolas.bortoli@tcheofertas.com.br>
2023-05-28 23:21:20 -0300

    Database connection using PDO

--- 05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
+++ 05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
@@ -1,11 +1,27 @@
 <?php
 require __DIR__ . '/../../fullstackphp/fsphp.php';
+require __DIR__ . "/../source/autoload.php";
+
 fullStackPHPClassName("05.03 - Errors, conexão e execução");
 
 /*
  * [ controle de erros ] http://php.net/manual/pt_BR/language.exceptions.php
  */
 fullStackPHPClassSession("controle de erros", __LINE__);
+
+try {
+    throw new Exception("Exception");
+
+    throw new PDOException("PDOException");
+} catch (PDOException $exception) {
+
+    var_dump($exception);
+
+} catch (Exception $exception) {
+
+    echo "<p class'trigger error'>{$exception->getMessage()}</p>";
+
+}
 
 
 /*
@@ -14,9 +30,41 @@ fullStackPHPClassSession("controle de erros", __LINE__);
  */
 fullStackPHPClassSession("php data object", __LINE__);
 
+try {
+    $pdo = new PDO (
+        "mysql:host=localhost;dbname=fsphp",
+        "root",
+        "",
+        [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]
+    );
+
+    $statement = $pdo->query("SELECT * FROM users LIMIT 3");
+    // Podemos alterar o comportamento do comando Fetch para retornar os dados no formato
+    // de array associativo.
+    while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
+        var_dump($user);
+    }
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
+
 
 /*
  * [ conexão com singleton ] Conextar e obter um objeto PDO garantindo instância única.
  * http://br.phptherightway.com/pages/Design-Patterns.html
  */
 fullStackPHPClassSession("conexão com singleton", __LINE__);
+
+use Source\Database\Connect;
+
+$PDO_connection_1 = Connect::getInstance();
+$PDO_connection_2 = Connect::getInstance();
+
+var_dump(
+    $PDO_connection_1,
+    $PDO_connection_2,
+    Connect::getInstance()
+);
--- 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
+++ 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
@@ -4,8 +4,6 @@ fullStackPHPClassName("05.04 - Consultas com query e exec");
 
 require __DIR__ . "/../source/autoload.php";
 
-use Source\Database\Connect;
-
 /*
  * [ insert ] Cadastrar dados.
  * https://mariadb.com/kb/en/library/insert/
@@ -15,6 +13,23 @@ use Source\Database\Connect;
  */
 fullStackPHPClassSession("insert", __LINE__);
 
+use Source\Database\Connect;
+
+$insert = "
+    INSERT INTO fsphp.users (first_name, last_name, email, document)
+    VALUES ('Nicolas', 'Bortoli', 'nicolas@hotmail.com', '03090988070');
+";
+
+try {
+
+    $exec = Connect::getInstance()->exec($insert);
+
+    var_dump(Connect::getInstance()->lastInsertId());
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
+
 
 /*
  * [ select ] Ler/Consultar dados.
@@ -22,6 +37,23 @@ fullStackPHPClassSession("insert", __LINE__);
  */
 fullStackPHPClassSession("select", __LINE__);
 
+$select = "
+    SELECT * FROM fsphp.users LIMIT 2; 
+";
+
+try {
+
+    $query = Connect::getInstance()->query($select);
+
+    var_dump([
+        $query,
+        $query->rowCount(),
+        $query->fetchAll()
+    ]);
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ update ] Atualizar dados.
@@ -29,9 +61,38 @@ fullStackPHPClassSession("select", __LINE__);
  */
 fullStackPHPClassSession("update", __LINE__);
 
+$update = "
+    UPDATE fsphp.users SET first_name = 'Kaue', last_name = 'Cardoso' WHERE id = '53';
+";
+
+try {
+
+    $exec = Connect::getInstance()->exec($update);
+
+    var_dump([
+        $exec,
+    ]);
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ delete ] Deletar dados.
  * https://mariadb.com/kb/en/library/delete/
  */
 fullStackPHPClassSession("delete", __LINE__);
+
+$delete = "
+    DELETE FROM fsphp.users WHERE id > 50;
+";
+
+try {
+
+    $exec = Connect::getInstance()->exec($delete);
+
+    var_dump($exec);
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
