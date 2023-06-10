3a21ea7571163527d2add152e5b5957a4e644eb2
Nicolas_note <nicolas.bortoli@tcheofertas.com.br>
2023-06-10 15:38:00 -0300

    Segurança e sessões

---
+++ 06-seguranca-e-boas-praticas/source/Core/Connect.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace Source\Core;
+
+use \PDO;
+use \PDOException;
+
+class Connect
+{
+    private const OPTIONS = [
+        // Assegura que ao iniciar a conexão com o DB o charset será utf8.
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        // Assegura que ao ocorrer um erro na aplicação será executada uma Exception do tipo PDO
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Indica que o retorno das querys irão vir em um formato de objeto.
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        // Assegura que a conexão com o banco irá respeitar o case das tabelas do DB.
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ];
+
+
+    // Vamos garantir que haverá apenas uma instancia de nossa classe de conexão
+    // na aplicação.
+    private static $instance;
+
+    // Nessa função asseguramos que caso não haja uma instancia da classe Connect, 
+    // uma conexão será executada, caso contrário, iremos retornar a instancia já existente.
+    public static function getInstance() : PDO
+    {
+        if (empty(self::$instance)) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=" . CONF_DB_HOST . ";dbname" . CONF_DB_NAME,
+                    CONF_DB_USER,
+                    CONF_DB_PASS,
+                    self::OPTIONS
+                );
+            } catch (PDOException $exception) {
+                die("<h1>Whoops, Erro ao conectar...</h1>");
+            }
+        }
+
+        return self::$instance;
+    }
+
+    final private function __construct()
+    {
+
+    }
+
+    final private function __clone()
+    {
+
+    }
+}
+
+?>
